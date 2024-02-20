1bc96b2e21fd8e6520cf76e88c184b9fbd5d5f0b
leonardo526 <leonardopiccioli936@gmail.com>
2024-02-20 10:50:49 +0100

    prove app e api con html marchi

---
+++ Api/proveLeoApp/provaApp.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request
+from api import *
+
+from Api.api import apiBlueprint
+from Api.proveLeoApp.provaApi import getAuto, getMarchio, getAutobyMarchio
+from Database.dbUtils import *
+
+app = Flask(__name__)
+app.register_blueprint(apiBlueprint)
+DBNAME = "concessionario"
+
+def select_specific_instance(table_name, instance_id):
+    c = create_db_connection(DBNAME)
+    query = f"SELECT * FROM {table_name} WHERE id = ?;"
+    result = read_query(c, query, (instance_id,))
+    c.close()
+    return result
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+
+
+@app.route('/auto')
+def show_auto():
+    param_ord = request.args.get('param_ord')
+    page = int(request.args.get('page', default=1))
+    items_per_page = 20
+    c = create_db_connection(DBNAME)
+    query = "SELECT COUNT(*) AS num_auto FROM auto"
+    conteggio = read_query(c, query)[0]['num_auto']
+    totale = (conteggio // items_per_page) + 1
+    marchi = request.args.get('marchi', default=None)
+
+    if marchi:
+        if isinstance(marchi, str):
+            data = getAutobyMarchio()
+        else:
+            raise TypeError("L'auto deve essere una stringa")
+    else:
+        if param_ord == "dal più basso":
+            data = getAuto(param_ord='asc')
+        elif param_ord == "dal più alto":
+            data = getAuto(param_ord='desc')
+        else:
+            data = getAuto()
+
+    c.close()
+    return render_template('auto.html', auto=data, page=page, total_pages=totale)
+
+
+
+@app.route('/marchi')
+def show_marchi():
+    page = int(request.args.get('page', default=1))
+    items_per_page = 10
+    c = create_db_connection(DBNAME)
+    query = "SELECT COUNT(*) AS num_marchi FROM marchi"
+    conteggio = read_query(c, query)[0]['num_generi']
+    totale = (conteggio // items_per_page) + 1
+    data = getMarchio()
+    return render_template('marchi.html', generi=data, page=page, total_pages=totale)
+
+
+# @app.route('/grafici')
+# def showGrafici():
+#     return render_template('Grafici.html')
+
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
