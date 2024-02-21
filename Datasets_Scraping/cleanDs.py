9cd85f1ee33e347e02ea650b8a22ecb756315ddb
Katowice <katerina978@protonmail.com>
2024-02-21 11:35:33 +0100

    fix del fix null pdisplacement

--- Datasets_Scraping/cleanDs.py
+++ Datasets_Scraping/cleanDs.py
@@ -30,7 +30,7 @@ def dai():
                         deletatiByCilindr += 1
                     elif pdisp == None and disp:
                         #print('DIIIIS', int(disp.split()[0])/1000)
-                        dcarModel_cln[brand][model]['engines'][eng]['Pdisplacement:'] = f"{int(disp.split()[0])/1000}L"
+                        dcarModel_cln[brand][model]['engines'][eng]['Pdisplacement'] = f"{int(disp.split()[0])/1000}L"
         print("cancellatiByEng", deletatiByEng)
         print("cancellatiByCilindr", deletatiByCilindr)
                     
