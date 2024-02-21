896e1fc237793c269a9d1abc68ab6b76349468de
Katowice <katerina978@protonmail.com>
2024-02-21 12:23:21 +0100

    guess? fix [json cleaner no empty engines]

--- Datasets_Scraping/cleanDs.py
+++ Datasets_Scraping/cleanDs.py
@@ -16,12 +16,19 @@ def dai():
                 a = dcarModel[brand][model]
                 if a['price'] < 1000:
                     dcarModel_cln[brand][model]['price'] = None
+                
+                if len(a['engines'].keys()) == 0:
+                    print("cancello [eng == 0]",brand, model)
+                    del dcarModel_cln[brand][model]
+                    deletatiByEng += 1
+                    continue
                 for eng in a['engines'].keys():
                     if len(a['engines'][eng].keys()) <3:
                         print("cancello [eng < 3]",brand, model, eng)
                         del dcarModel_cln[brand][model]['engines'][eng]
                         deletatiByEng += 1
-
+                        continue
+                
                     disp = a['engines'][eng].get('Displacement:')
                     pdisp = a['engines'][eng].get('Pdisplacement')
                     if disp == None and pdisp == None:
@@ -32,6 +39,12 @@ def dai():
                         #print('DIIIIS', int(disp.split()[0])/1000)
                         numz =  "{:.1f}".format(int(disp.split()[0])/1000) 
                         dcarModel_cln[brand][model]['engines'][eng]['Pdisplacement'] = f"{numz}L"
+                if len(dcarModel_cln[brand][model]['engines'].keys()) == 0:
+                    print("cancello [eng == 0]",brand, model)
+                    del dcarModel_cln[brand][model]
+                    deletatiByEng += 1
+                    continue
+                
         print("cancellatiByEng", deletatiByEng)
         print("cancellatiByCilindr", deletatiByCilindr)
                     
