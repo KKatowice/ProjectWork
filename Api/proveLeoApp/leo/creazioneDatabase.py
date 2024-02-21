701aefac1278d34095c56b8646f99064036b8b7c
leonardo526 <leonardopiccioli936@gmail.com>
2024-02-21 14:44:00 +0100

    changes api, app, create table

---
+++ Api/proveLeoApp/leo/creazioneDatabase.py
@@ -0,0 +1,63 @@
+from Database.dbUtils import *
+
+connection = create_server_connection()
+create_database(connection, "CREATE DATABASE concessionario")
+c = create_db_connection("concessionario")
+
+marchi = """CREATE TABLE marchi(
+            id_marchio int PRIMARY KEY AUTO_INCREMENT,
+            nome varchar(45) UNIQUE NOT NULL,
+            foto_marchio varchar(255))"""
+
+motore = """CREATE TABLE motori(
+            id_motore int PRIMARY KEY AUTO_INCREMENT,
+            cilindrata int,
+            potenza int,
+            cavalli int,
+            carburante varchar(25),
+            consumi decimal(10,2),
+            emissioni decimal(10,2),
+            serbatoio decimal(10,2)
+            )"""
+
+auto = """CREATE TABLE auto(
+            id_auto int PRIMARY KEY AUTO_INCREMENT,
+            id_motore int,
+            id_marchio int,
+            modello varchar(55),
+            prezzo decimal(10,2), 
+            foto_auto varchar(255),
+            FOREIGN KEY (id_motore) REFERENCES motori(id_motore) ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY (id_marchio) REFERENCES marchi(id_marchio) ON DELETE CASCADE ON UPDATE CASCADE)"""
+
+utenti = """CREATE TABLE utenti(
+            id_utente int PRIMARY KEY AUTO_INCREMENT,
+            nome varchar(55) NOT NULL,
+            cognome varchar(55) NOT NULL,
+            eta int(11) NOT NULL CHECK(eta>0 AND eta<120),
+            sesso varchar(55) NOT NULL CHECK(sesso = 'maschio' or sesso ='femmina' or sesso = 'altro'),
+            email varchar(55) UNIQUE NOT NULL,
+            password varchar(55) UNIQUE NOT NULL,
+            cap varchar(6),
+            budget int)"""
+
+preferenze = """CREATE TABLE preferenze(
+                id_preferenze int PRIMARY KEY AUTO_INCREMENT,
+                id_auto int,
+                id_utente int,
+                FOREIGN KEY (id_auto) REFERENCES auto(id_auto) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (id_utente) REFERENCES utenti(id_utente) ON DELETE CASCADE ON UPDATE CASCADE)"""
+
+
+utenti_bloccati = """CREATE TABLE utenti_bloccati(
+                    id_utente int FOREIGN KEY (id_utente) REFERENCES utente(id_utente) ON DELETE CASCADE ON UPDATE CASCADE,
+                    password varchar(55) UNIQUE NOT NULL)
+                    """
+
+
+execute_query(c, marchi)
+execute_query(c, motore)
+execute_query(c, auto)
+execute_query(c, utenti)
+execute_query(c, preferenze)
+execute_query(c, utenti_bloccati)
