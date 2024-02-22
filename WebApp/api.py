10847ab0e7bbdd4a74842a99a62e4191ae506ecc
peppo95 <edop95@gmail.com>
2024-02-22 09:30:55 +0100

    print di prova

--- WebApp/api.py
+++ WebApp/api.py
@@ -122,7 +122,7 @@ def filtra_auto():
         data = request.get_json()
     except Error as e:
         print(e)
-    print("success")
+    print(data)
     q = (Auto.query.join(Motore, Motore.id_motore == Auto.id_motore).join(Marchio, Marchio.id_marchio == Auto.id_marchio).filter(Marchio.nome == data['marchio']).filter(Motore.carburante == data['carburante']).filter(Motore.consumi < float(data['consumi'])).filter(Motore.emissioni < float(data['emissioni'])).filter(Auto.prezzo < float(data['prezzo'])).filter(Motore.serbatoio > float(data['serbatoio'])).filter(Motore.potenza > int(data['potenza'])).filter(Motore.cilindrata > int(data['cilindrata'])).filter(Motore.cavalli > int(data['cavalli'])))
     print("success")
     result = q.all()
